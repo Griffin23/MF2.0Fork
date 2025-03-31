1613afeba8cb1d8747120b03819895857228a0db
Michael Sun <siquan.sun@rockwellautomation.com>
2025-04-01 07:37:22 +0800

    mfe1 mf 2.0

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,15 +1,38 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+// const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  ModuleFederationPlugin,
+} = require("@module-federation/enhanced/webpack");
 
-module.exports = withModuleFederationPlugin({
+// module.exports = withModuleFederationPlugin({
 
-  name: 'mfe1',
+//   name: 'mfe1',
 
-  exposes: {
-    './Module': './projects/mfe1/src/app/flights/flights.module.ts',
-  },
+//   exposes: {
+//     './Module': './projects/mfe1/src/app/flights/flights.module.ts',
+//   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+//   shared: {
+//     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+//   },
 
-});
+// });
+
+module.exports = {
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "mfe1",
+      filename: 'remoteEntry.js',
+      exposes: {
+        "./Module": "./projects/mfe1/src/app/flights/flights.module.ts",
+      },
+
+      shared: {
+        // ...shareAll({
+        //   singleton: true,
+        //   strictVersion: true,
+        //   requiredVersion: "auto",
+        // }),
+      },
+    }),
+  ],
+};
