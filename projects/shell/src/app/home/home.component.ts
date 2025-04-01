6c7641b1fb3ce55dcc8ed86cea9f273e508fd3ea
Michael Sun <siquan.sun@rockwellautomation.com>
2025-04-01 14:02:15 +0800

    fix build error

--- projects/shell/src/app/home/home.component.ts
+++ projects/shell/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { initFederation } from '@angular-architects/module-federation';
+// import { initFederation } from '@angular-architects/module-federation';
+import { init, registerRemotes, loadRemote } from '@module-federation/enhanced/runtime';
+
 
 @Component({
     selector: 'app-home',
@@ -12,7 +14,22 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     console.log('*****');
-    initFederation({});
+    // initFederation({});
+    init({
+      name: 'my_host',
+      remotes: []
+    });
+    
+    // export type RemoteEntryType = 'var' | 'module' | 'assign' | 'assign-properties' | 'this' | 'window' | 'self' | 'global' | 'commonjs' | 'commonjs2' | 'commonjs-module' | 'commonjs-static' | 'amd' | 'amd-require' | 'umd' | 'umd2' | 'jsonp' | 'system' | string;
+    registerRemotes([{
+      name: 'mfe1',
+      type: 'module',
+      entry: 'http://localhost:3000/remoteEntry.js'
+    }]);
+
+    loadRemote('mfe1/Module').then(r => {
+      console.log('-----------------------', r);
+    });
   }
 
 }
